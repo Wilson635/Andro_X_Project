8eca5367e16ee12d68ecc5a56738447ca28280a6
wilson635 <ngahemeniw@gmail.com>
2022-08-10 11:57:45 +0100

    Added project

---
+++ app/src/main/java/com/example/info/RegisterActivity.java
@@ -0,0 +1,103 @@
+package com.example.info;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private boolean passwordShowing = false;
+    private boolean conPasswordShowing = false;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        final EditText email = findViewById(R.id.emailET);
+        final EditText mobile = findViewById(R.id.mobileET);
+
+        final EditText password = findViewById(R.id.passwordET);
+        final EditText conPassword = findViewById(R.id.conPasswordET);
+        final ImageView passwordIcon = findViewById(R.id.passwordIcon);
+        final ImageView conPasswordIcon = findViewById(R.id.conPasswordIcon);
+
+        final AppCompatButton signUpBtn = findViewById(R.id.signUpBtn);
+        final TextView signInBtn = findViewById(R.id.signInBtn);
+
+        passwordIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //checking if a password is showing or not
+                if (passwordShowing) {
+                    passwordShowing = false;
+
+                    password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    passwordIcon.setImageResource(R.drawable.eyeslash);
+                }
+                else {
+                    passwordShowing = true;
+
+                    password.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                    passwordIcon.setImageResource(R.drawable.eye);
+                }
+
+                //move the cursor at last of the text
+                password.setSelection(password.length());
+            }
+        });
+
+        conPasswordIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //checking if a password is showing or not
+                if (conPasswordShowing) {
+                    conPasswordShowing = false;
+
+                    conPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    conPasswordIcon.setImageResource(R.drawable.eyeslash);
+                }
+                else {
+                    conPasswordShowing = true;
+
+                    conPassword.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                    conPasswordIcon.setImageResource(R.drawable.eye);
+                }
+
+                //move the cursor at last of the text
+                conPassword.setSelection(conPassword.length());
+            }
+        });
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                final String getMobileTxt = mobile.getText().toString();
+                final String getEmailTxt = email.getText().toString();
+
+                //opening OTP verification Activity along with mobile and email
+                Intent intent = new Intent(RegisterActivity.this, Validation.class);
+
+                intent.putExtra("mobile", getMobileTxt);
+                intent.putExtra("email", getEmailTxt);
+
+                startActivity(intent);
+            }
+        });
+
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/info/Validation.java
@@ -0,0 +1,188 @@
+package com.example.info;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Validation extends AppCompatActivity {
+
+    private EditText otpET1, otpET2, otpET3, otpET4;
+    private TextView resendBtn;
+
+    //true after every 60 seconds
+    private boolean resendEnabled = false;
+
+    //resend time in seconds
+    private int resendTime = 60;
+
+    private int selectedETPosition = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_validation);
+
+        otpET1 = findViewById(R.id.otpET1);
+        otpET2 = findViewById(R.id.otpET2);
+        otpET3 = findViewById(R.id.otpET3);
+        otpET4 = findViewById(R.id.otpET4);
+
+        resendBtn = findViewById(R.id.resendBtn);
+        final Button verifyBtn = findViewById(R.id.verifyBtn);
+
+        final TextView otpEmail = findViewById(R.id.otpEmail);
+        final TextView otpMobile = findViewById(R.id.otpMobile);
+
+        //Getting email and mobile from register activity through indent
+        final String getEmail = getIntent().getStringExtra("email");
+        final String getMobile = getIntent().getStringExtra("mobile");
+
+        //setting email and mobile to TextView
+        otpEmail.setText(getEmail);
+        otpMobile.setText(getMobile);
+
+        otpET1.addTextChangedListener(textWatcher);
+        otpET2.addTextChangedListener(textWatcher);
+        otpET3.addTextChangedListener(textWatcher);
+        otpET4.addTextChangedListener(textWatcher);
+
+        // by default open keyboard at otpET1
+        showKeyBoard(otpET1);
+
+        //start resend count down timer
+        startCountDownTimer();
+
+        resendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(resendEnabled) {
+
+                    //handle your resend code here
+
+                    //start new resend count down timer
+                    startCountDownTimer();
+                }
+            }
+        });
+
+        verifyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                final String generateotp = otpET1.getText().toString()+otpET2.getText().toString()+otpET3.getText().toString()+otpET4.getText().toString();
+
+                if(generateotp.length() == 4) {
+
+                    //handle your otp verification here
+                }
+            }
+        });
+
+        verifyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Validation.this, Splash.class));
+            }
+        });
+
+    }
+
+    private void showKeyBoard(EditText otpET) {
+
+        otpET.requestFocus();
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputMethodManager.showSoftInput(otpET, InputMethodManager.SHOW_IMPLICIT);
+    }
+
+    private void startCountDownTimer() {
+
+        resendEnabled = false;
+        resendBtn.setTextColor(Color.parseColor("#99000000"));
+
+        new CountDownTimer(resendTime * 1000, 1000) {
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                resendBtn.setText("Resend Code("+(millisUntilFinished / 1000)+")");
+            }
+
+            @Override
+            public void onFinish() {
+                resendEnabled = true;
+                resendBtn.setText("Resend Code");
+                resendBtn.setTextColor(getResources().getColor(R.color.primary));
+            }
+        }.start();
+    }
+    private final TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+            if(toString().length() > 0) {
+
+                if(selectedETPosition == 0) {
+                    selectedETPosition = 1;
+                    showKeyBoard(otpET2);
+                } 
+                else if (selectedETPosition == 1) {
+                    selectedETPosition = 2;
+                    showKeyBoard(otpET3);
+                } 
+                else if (selectedETPosition == 2) {
+                    selectedETPosition = 3;
+                    showKeyBoard(otpET4);
+                }
+            }
+        }
+    };
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+
+        if(keyCode == KeyEvent.KEYCODE_DEL) {
+
+            if(selectedETPosition == 3) {
+
+                selectedETPosition = 2;
+                showKeyBoard(otpET3);
+            } else if (selectedETPosition == 2) {
+                selectedETPosition = 1;
+                showKeyBoard(otpET2);
+            } else if (selectedETPosition == 1) {
+                selectedETPosition = 0;
+                showKeyBoard(otpET1);
+            }
+
+            return true;
+        }
+        else {
+            return super.onKeyUp(keyCode, event);
+        }
+
+
+    }
+
+
+}
